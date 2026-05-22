929db84aff984a1941d6dc30a00bb9fa1d83d929
Maryna Melnyk <marynamelnyk24@gmail.com>
2026-05-22 17:34:12 +0300

    Add response schema validation layer for API contract checks

--- tests/posts/test_create_post.py
+++ tests/posts/test_create_post.py
@@ -3,6 +3,7 @@ from pytest import assume
 from data.test_data import CREATE_POST_PAYLOAD
 from endpoints.posts import PostsAPI
 from utils.assertions import Assert
+from utils.schemas import PostSchema
 
 
 def test_create_post(posts_api: PostsAPI):
@@ -12,6 +13,7 @@ def test_create_post(posts_api: PostsAPI):
     response_json = response.json()
 
     Assert.status_code(response, 201)
+    PostSchema.validate(response_json)
 
     with assume:
         Assert.equals(
@@ -26,6 +28,3 @@ def test_create_post(posts_api: PostsAPI):
             payload["body"],
             "body"
         )
-
-    with assume:
-        Assert.has_key(response_json, "id")
--- tests/posts/test_get_posts.py
+++ tests/posts/test_get_posts.py
@@ -1,22 +1,31 @@
 from pytest import assume
 
 from utils.assertions import Assert
+from utils.schemas import PostSchema
 
 
 def test_get_posts(posts_api):
     response = posts_api.get_posts()
+    response_json = response.json()
 
     Assert.status_code(response, 200)
 
+    assert isinstance(response_json, list)
+    assert response_json, "Response should not be empty"
+
+    for post in response_json[:3]:
+        PostSchema.validate(post)
 
 def test_get_single_post(posts_api):
     response = posts_api.get_post(1)
+    response_json = response.json()
 
     Assert.status_code(response, 200)
+    PostSchema.validate(response_json)
 
     with assume:
         Assert.equals(
-            response.json()["id"],
+            response_json["id"],
             1,
             "id"
         )
